c8297cd45d24d726c6a4a70acf62bb6547c87892
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-05-06 16:52:11 +0200

    Add small and large image variants URLs to /products and /products/:id endpoints

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,17 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     render json: @products.to_json(
-      except: [:description]
+      except: [:description],
+      methods: :image_sm,
     )
   end
 
   def show
     @product = Product.find_by(id: params[:id])
     if @product
-      render json: @product
+      render json: @product.to_json(
+        methods: :image_lg,
+      )
     else
       render json: {
         errors: ["Product not found"]
